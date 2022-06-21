126cc4befac18bac72af9fa974eaf09abd1a2ce1
axitvadi-admin <axit.vadi@infyom.com>
2022-06-21 14:52:00 +0530

    chore: iinitial setup

---
+++ config/db.js
@@ -0,0 +1,20 @@
+
+const fastifyPlugin = require('fastify-plugin');
+
+async function dbConnect(fastify,option){
+
+    const mongoose = require('mongoose')
+
+    mongoose.connect(process.env.MONGO_URL);
+
+    const db = mongoose.connection;
+
+    db.on('error',console.error.bind(console,"failed to connect database"));
+
+    db.once('open',()=>{
+        console.log("successfully connected to database..");
+    })
+
+}
+
+module.exports = fastifyPlugin(dbConnect);
---
+++ model/countryStateCity.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const countryStateCitySchema = new Schema({
+    country:{
+        type:String
+    },
+    state:{
+        type:Number
+    },
+
+});
+
+module.exports = mongoose.model('Product',countryStateCitySchema);
