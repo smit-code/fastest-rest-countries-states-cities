4f67a323254d43c11aa5f0faafd0c27fb778fb6d
axitvadi-admin <axit.vadi@infyom.com>
2022-06-27 15:43:30 +0530

    fix: commented code removed

--- utils/swagger.js
+++ utils/swagger.js
@@ -1,34 +1,4 @@
 const path = __dirname + '/swagger.json';
-// exports.swaggerOptions = {
-//     routePrefix: '/doc',
-//     mode: 'static',
-//     specification: {
-//         path: path,
-//         postProcessor: function (swaggerObject) {
-//             return swaggerObject
-//         }
-//     },
-//     // swagger: {
-//
-//         // info: {
-//         //     title: ' Fastest Api for Countries, States, Cities',
-//         //     description: 'Get Fastest Response from Our Api...',
-//         //     version: '0.1.0'
-//         // },
-//         // externalDocs: {
-//         //     url: 'https://swagger.io',
-//         //     description: 'Find more info here'
-//         // },
-//         // host: '127.0.0.1:3000',
-//         // schemes: ['http'],
-//         // consumes: ['application/json'],
-//         // produces: ['application/json']
-//
-//     // },
-//     exposeRoute: true
-// }
-
-
 exports.swaggerOptions = {
     routePrefix:'/doc',
     mode: 'static',
