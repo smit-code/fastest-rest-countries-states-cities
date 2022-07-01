3681b835877ecb831b1a39cdc2fd4b955227da06
Smit <smit.gajera@infyom.com>
2022-07-01 18:28:48 +0530

    fix: minor changes

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ fastify.setErrorHandler(function (error, request, reply) {
 })
 
 // Run the server!
-fastify.listen(process.env.PORT, '0.0.0.0', (err, address) => {
+await fastify.listen(process.env.PORT, '0.0.0.0', (err, address) => {
     if (err) throw err
     fastify.log.info(`server listening on ${address}`)
 })
