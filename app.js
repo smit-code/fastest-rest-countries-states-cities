b8e220402376401ff1bec8b3ab925bbe0ae73932
Smit <smit.gajera@infyom.com>
2022-07-01 18:20:49 +0530

    fix: minor changes

--- app.js
+++ app.js
@@ -6,57 +6,6 @@ const fastify = Fastify({
 const {swaggerOptions} = require("./utils/swagger");
 const swagger = require("@fastify/swagger");
 
-// env configuration
-// const fastifyEnv = require('@fastify/env')
-// const schema = {
-//     type: 'object',
-//     properties: {
-//         MONGO_URL: {
-//             type: 'string'
-//         },
-//         PORT: {
-//             type: 'number'
-//         }
-//     },
-//     required: ['MONGO_URL', 'PORT']
-// }
-// const options = {
-//     dotenv: true,
-//     confKey: 'config',
-//     schema: schema,
-// }
-//
-// fastify.register(fastifyEnv, options).ready((err, data) => {
-// // Run the server!
-//     const PORT = process.env.PORT;
-//
-//     const start = async () => {
-//         try {
-//             await fastify.listen({port: PORT})
-//         } catch (err) {
-//             fastify.log.error(err)
-//             process.exit(1)
-//         }
-//     }
-//     start()
-// })
-
-fastify.register(require('@fastify/cors'), function (instance) {
-    return (req, callback) => {
-        let corsOptions;
-        // const origin = req.headers.host
-        // console.log("this is origin",origin);
-        // do not include CORS headers for requests from localhost
-        const hostname = req.headers.host;
-        if (hostname === "localhost" && hostname === "127.0.0.1:3000") {
-            corsOptions = {origin: false}
-        } else {
-            corsOptions = {origin: true}
-        }
-        callback(null, corsOptions) // callback expects two parameters: error and options
-    }
-})
-
 //db Connection
 fastify.register(require('./config/db'));
 
@@ -76,12 +25,7 @@ fastify.setErrorHandler(function (error, request, reply) {
 })
 
 // Run the server!
-const ADDRESS = "0.0.0.0";
-const PORT = process.env.PORT || 3000;
-
-fastify.listen(PORT, ADDRESS, (err, address) => {
-    if (err) {
-        console.log(err);
-        process.exit(1);
-    }
-});
+fastify.listen(process.env.PORT, '0.0.0.0', (err, address) => {
+    if (err) throw err
+    fastify.log.info(`server listening on ${address}`)
+})
